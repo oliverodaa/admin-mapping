1c35de26114cb7c214d759550fd2f8f818d4e828
Ollie O'Donnell <oliverodaa@gmail.com>
2016-03-23 15:12:08 -0700

    errs

--- app/routes/defaultRoute.js
+++ app/routes/defaultRoute.js
@@ -2,4 +2,8 @@ var controllers = require('../controllers/controllers.js');
 
 module.exports = function (app) {
   app.get('/', renderPage);
+  // Error handling
+  app.use(function(err, req, res, next) {
+    res.status(500).send('Something broke!');
+  })
 };
