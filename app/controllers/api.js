aa8054d9bb0cb3c36abe01312646abc9f8e0244c
Ollie O'Donnell <oliverodaa@gmail.com>
2016-03-25 23:33:23 -0700

    Modify dummy data

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,42 +1,38 @@
+var response = {
+  data:"not implemented"
+};
 module.exports = {
   // Returns list of people
   people: function(req, res, next) {
-    var json = {
-      hey:"hi",
-      hello:1234
-    };
-    res.send(json);
+    var dbData = [
+      {
+        "person_id":"pers_7",
+        "name":"John a. powell",
+        "gender":"m"
+      },
+      {
+        "person_id":"pers_8",
+        "name":"Victoria Robinson",
+        "gender":"f"
+      }
+    ];
+    res.send(dbData);
   },
   // Returns all data for specific person_id
   person: function(req, res, next) {
-    var json = {
-      hey:"hi",
-      hello:1234
-    };
-    res.send(json);
+    // Look for
+    res.send(response);
   },
   // Returns list of positions
   positions: function(req, res, next) {
-    var json = {
-      hey:"hi",
-      hello:1234
-    };
-    res.send(json);
+    res.send(response);
   },
   // Returns all data for specific position
   position: function(req, res, next) {
-    var json = {
-      hey:"hi",
-      hello:1234
-    };
-    res.send(json);
+    res.send(response);
   },
   // Returns list of roles
   roles: function(req, res, next) {
-    var json = {
-      hey:"hi",
-      hello:1234
-    };
-    res.send(json);
+    res.send(response);
   }
 };
