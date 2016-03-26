efc5d37fc2c045b1f553eef532c613b6af6332e0
Ollie O'Donnell <oliverodaa@gmail.com>
2016-03-25 17:41:18 -0700

    fixed controllers

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,9 +1,22 @@
+var locals = {
+        title: "Know Your University",
+        footerText: "Website made by Ollie O'Donnell in Berkeley, CA"
+      };
+
 module.exports = {
   renderPage: function(req, res, next) {
-    res.render('simple',
-      {
-        title: "Know Your University",
-        footerText: "Footer Text"
-      });
+    res.render('simple',locals);
+  },
+  home: function(req, res, next) {
+    res.render('home',locals);
+  },
+  people: function(req, res, next) {
+    res.render('people',locals);
+  },
+  positions: function(req, res, next) {
+    res.render('positions',locals);
+  },
+  roles: function(req, res, next) {
+    res.render('roles',locals);
   }
 };
