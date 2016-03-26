46b9859c7c004e9138bedfc80eef2cdd62a6d5e3
Ollie O'Donnell <oliverodaa@gmail.com>
2016-03-25 17:32:27 -0700

    New routes

--- app/routes/defaultRoute.js
+++ app/routes/defaultRoute.js
@@ -1,7 +1,22 @@
 var controllers = require('../controllers/controllers.js');
 
 module.exports = function (app) {
-  app.get('/', controllers.renderPage);
+  /**
+   * Rendering Views
+   */
+  app.get('/', controllers.home);
+
+  app.get('/people', controllers.people);
+  app.get('/people/:id', controllers.people);
+
+  app.get('/positions', controllers.positions);
+  app.get('/positions/:id', controllers.people);
+
+  app.get('/roles', controllers.roles);
+  /**
+   * API Responses
+   */
+
   // Error handling
   app.use(function(err, req, res, next) {
     res.status(500).send('Something broke!');
